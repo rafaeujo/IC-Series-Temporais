395cddba0966fc9c9130f2cfa8003c3efe2d77bc
rafaeujo <rafael.js.oliveira>
2025-07-02 14:06:36 -0300

    asdasdava

--- Rafael/n = 100/Deeplearning teste 2.R
+++ Rafael/n = 100/Deeplearning teste 2.R
@@ -11,6 +11,17 @@ metricas_deep <- function(alfa, data, coluna, antigo) {
 
 
 ## Calculando RMSE
+deep.puro <- read.csv('Rafael/dados_imputados_1.csv')
+medidasAR04.10.100.1 <- metricas_deep(10, deep2, "Dado10", deep2$Coluna.controle)
+print(medidasAR04.10.100.1)
 
-medidasAR04.10.100.2 <- metricas_deep(10, deep, "Dado10", deep2$Coluna.controle)
-print(medidasAR04.10.100)
+medidasAR04.10.100.2 <- metricas_deep(10, deep2, "Dado10", deep2$Coluna.controle)
+print(medidasAR04.10.100.2)
+
+deep3 <- read.csv('Rafael/dados_imputados_cross.csv')
+medidasAR04.10.100.3 <- metricas_deep(10, deep3, "Dado10", deep3$Coluna.controle)
+print(medidasAR04.10.100.3)
+
+deeplag <- read.csv('Rafael/dados_imputados_lag.csv')
+medidasAR04.10.100.lag <- metricas_deep(10, deeplag, "Dado10", deeplag$Coluna.controle)
+print(medidasAR04.10.100.lag)
